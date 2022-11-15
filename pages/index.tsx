ffcf8c3807d1821252e89040eae5db58c44a4444
ahnaf <ahnaf@mail.local>
2022-11-15 18:55:22 +0700

    init landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,83 +1,218 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next"
+import Head from "next/head"
+import Image from "next/image"
+import { Element, Link } from "react-scroll"
+
+const features = [
+  {
+    media: "/triangle.svg",
+    title: "Hunt",
+    description: "Find the rarest and limited real-world collections in town",
+  },
+  {
+    media: "/cross.svg",
+    title: "Levelling",
+    description:
+      "As you collect more items, you'll level up and unlock even more fun features!",
+  },
+  {
+    media: "/circle.svg",
+    title: "Tradeable",
+    description: "All items in this app are tradable in any NFT marketplaces ",
+  },
+  {
+    media: "/rectangle.svg",
+    title: "NFT",
+    description:
+      "We uses NFT technology so all the items authentic and stored digitally.",
+  },
+]
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-black">
       <Head>
-        <title>Create Next App</title>
+        <title>Geovox - Find and collect items while you travel</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <nav className="w-full max-w-5xl m-auto">
+        <div className="px-4 md:px-0 py-4 flex flex-col md:flex-row justify-between items-center">
+          <Image src="/logo-g.png" alt="logo-g" width={25} height={25} />
+          <div className="flex gap-4 md:gap-6 mt-4 md:mt-0">
+            <Link
+              activeClass="active"
+              to="features"
+              spy={true}
+              smooth={true}
+              duration={500}
+            >
+              <p className="cursor-pointer">Features</p>
+            </Link>
+            <Link
+              activeClass="active"
+              to="apps"
+              spy={true}
+              smooth={true}
+              duration={500}
+            >
+              <p className="cursor-pointer">Apps</p>
+            </Link>
+            <Link
+              activeClass="active"
+              to="download"
+              spy={true}
+              smooth={true}
+              duration={500}
+            >
+              <p className="cursor-pointer">Download</p>
+            </Link>
+          </div>
         </div>
+      </nav>
+
+      <main className="flex w-full flex-1 flex-col items-center justify-center my-4 md:my-12 p-4 md:p-0">
+        <div className="md:flex justify-between my-4 md:my-16 max-w-5xl m-auto">
+          <div className="flex-1">
+            <p className="font-bold text-4xl whitespace-pre-line">
+              {"Find and collect\n items while you travel"}
+            </p>
+            <p className="my-4 font-normal text-gray-200">
+              Are you a traveler, or do you just love to travel?
+            </p>
+            <p className="my-4 font-normal text-gray-200">
+              Collect Items for your greatest adventure. It's a real-world
+              location app that lets you hunt for NFT items while you're out and
+              about.
+            </p>
+            <Link
+              activeClass="active"
+              to="download"
+              spy={true}
+              smooth={true}
+              duration={500}
+            >
+              <button className="bg-primary-orange px-4 py-2.5 mt-4 rounded-2xl mb-6">
+                <div className="flex items-center justify-center gap-2 text-black font-bold">
+                  Download App
+                </div>
+              </button>
+            </Link>
+          </div>
+          <div className="flex-1 flex justify-end">
+            <Image
+              src="/people-telescope.svg"
+              alt="People telescope"
+              width={400}
+              height={300}
+              priority
+            />
+          </div>
+        </div>
+
+        <Element className="my-12 max-w-5xl m-auto" name="features">
+          <p className="font-bold text-2xl text-white text-center">Features</p>
+          <div className="m-auto w-16 h-1.5 bg-primary-orange mb-6" />
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-6">
+            {features.map((feature, index) => (
+              <div
+                key={index}
+                className="flex flex-col justify-between items-center bg-primary-gray-150 px-4 py-6 md:px-6 md:py-8 gap-y-2 rounded-2xl"
+              >
+                <Image
+                  src={feature.media}
+                  alt={feature.title}
+                  width={90}
+                  height={90}
+                />
+                <div>
+                  <p className="mt-4 font-bold text-xl text-center">
+                    {feature.title}
+                  </p>
+                  <p className="mt-2 text-center text-gray-200 text-sm">
+                    {feature.description}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </Element>
+
+        <Element className="my-12 max-w-5xl m-auto" name="apps">
+          <p className="font-bold text-2xl text-white">Apps</p>
+          <div className="w-16 h-1.5 bg-primary-orange mb-6" />
+          <div className="my-8 grid grid-cols-2 md:grid-cols-4 gap-4">
+            <Image
+              src="/screenshot/home.PNG"
+              alt="Crossing guy"
+              width={300}
+              height={300}
+            />
+            <Image
+              src="/screenshot/map.PNG"
+              alt="Crossing guy"
+              width={300}
+              height={300}
+            />
+            <Image
+              src="/screenshot/detail.PNG"
+              alt="Crossing guy"
+              width={300}
+              height={300}
+            />
+            <Image
+              src="/screenshot/profile.PNG"
+              alt="Crossing guy"
+              width={300}
+              height={300}
+            />
+          </div>
+        </Element>
+
+        <Element
+          className="my-12 w-full relative py-12 max-w-5xl"
+          name="download"
+        >
+          <div className="absolute inset-0 md:w-2/3 -ml-4 md:ml-0 rounded-tr-2xl rounded-br-2xl bg-primary-orange z-0" />
+          <div className="w-full max-w-4xl m-auto relative z-10 md:flex p-4 md:p-0">
+            <div className="text-black md:w-1/2">
+              <p className="font-bold text-4xl whitespace-pre-line">
+                Try our Beta App
+              </p>
+              <p className="my-4">
+                Are you ready to get a first look at the beta release of Geovox?
+                We're so excited for you to check it out! we'd love to hear your
+                feedback!
+              </p>
+              <p className="my-4">
+                Let us know what you think—and please feel free to let us know
+                if there's anything else we can add or any features that are
+                missing from our app.
+              </p>
+              <div className="mt-8 flex gap-4">
+                <p className="font-semibold text-xl underline underline-offset-4">
+                  Android
+                </p>
+                <p className="font-semibold text-xl underline underline-offset-4">
+                  iOS
+                </p>
+              </div>
+            </div>
+            <div className="mt-8 md:mt-0 md:w-1/2 flex justify-center">
+              <Image
+                src="/crossing-guy.svg"
+                alt="Crossing guy"
+                width={300}
+                height={300}
+              />
+            </div>
+          </div>
+        </Element>
       </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
+      <footer className="flex flex-col p-4 h-24 w-full items-center justify-center my-12 pointer-events-none">
+        <Image src="/Geovox.png" alt="Geovox Logo" width={230} height={50} />
+        <p className="text-white mt-4">Copyright 2022 Geovox</p>
       </footer>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          orange: "#FCA311",
+          gray: {
+            100: "#111111",
+            150: "#151515",
+            200: "#222222",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
